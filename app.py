1fc528783a1b4b09e9a379090b036f682de33139
kakadeamrut71-lang <kakadeamrut71@gmail.com>
2026-06-01 16:40:27 +0530

    app.py

--- app.py
+++ app.py
@@ -140,5 +140,5 @@ def view_cart():
 if __name__ == "__main__":
     import os
 
-       port = int(os.environ.get("PORT", 5000))
+port = int(os.environ.get("PORT", 5000))
        app.run(host="0.0.0.0", port=port)
